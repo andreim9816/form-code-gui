10fb254f778ef1ba4b6dd73fbb4d41001fa1e076
Andrei Manolache <andreim98@yahoo.com>
2025-06-08 12:50:04 +0300

    if a request doesn't finish in 15s, then the loader component should still work

--- src/app/http/HttpRequestInterceptor.ts
+++ src/app/http/HttpRequestInterceptor.ts
@@ -1,28 +1,26 @@
 import {HttpInterceptorFn} from '@angular/common/http';
-import {finalize} from 'rxjs';
+import {finalize, timeout} from 'rxjs';
 import {inject} from '@angular/core';
 import {LoadingService} from './LoadingService';
 
-let totalRequests = 0;
-let requestsCompleted = 0;
+let activeRequests = 0;
 
 export const httpRequestInterceptor: HttpInterceptorFn = (req, next) => {
   const loader = inject(LoadingService);
 
-  loader.show();
-  totalRequests++;
+  if (activeRequests === 0) {
+    loader.show();
+  }
+
+  activeRequests++;
 
   return next(req).pipe(
+    timeout(15000), // if a request takes too long, then after 15s it should throw an error
     finalize(() => {
-      requestsCompleted++;
-
-      // console.log(requestsCompleted, totalRequests);
-
-      if (requestsCompleted === totalRequests) {
+      activeRequests--;
+      if (activeRequests === 0) {
         loader.hide();
-        totalRequests = 0;
-        requestsCompleted = 0;
       }
     })
   );
-};
+}
